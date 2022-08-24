b0481bdf37cdafa039e427d0a1d3747f2317e2b4
Trevor-eby <112001674+Trevor-eby@users.noreply.github.com>
2022-08-24 11:47:47 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**********************************************************
+* Hello.java
+***********************************************************
+* Trevor Eby
+* 8/24/2022
+* CMSC-255-004
+***********************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Trevor Eby>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Displays information about me, this calss, and this program.>");
+		System.out.println("<8/24/2022>");
+		System.out.println("<CMSC-255-004>");
+		System.out.println();
+	}
+}
